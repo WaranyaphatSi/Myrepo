c43ffde5c3732c872bb271a70f3f1e97e4e51be4
WaranyaphatSi <63160294@go.buu.ac.th>
2021-12-17 17:24:44 +0700

    play

--- welcome_message.php
+++ welcome_message.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Heading 1 to 6</title>
+<title>Software Engineering</title>
+<meta charset="utf-8">
 </head>
 <body>
-	line 1 <br>
-	line 2
-	line 3
-<h1 align = "center">Heading 1</h1>
-<h2 align = "center">Heading 2</h2>
-<h3>Heading 3</h3>
-<h4>Heading 4</h4>
-<h5>Heading 5</h5>
-<h6>Heading 6</h6>
+<h1 align="center">Burapha University</h1>
+<hr>
+<p align="center">
+Faculty of Informatics
+<br>
+Training Camp
+</p>
 </body>
-</html>
