d3688484e43560bfbbc939eae88253a5c4f6ec17
WaranyaphatSi <63160294@go.buu.ac.th>
2021-12-17 17:42:47 +0700

    play

--- welcome_message.php
+++ welcome_message.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>font face</title>
+<title>font color</title>
 </head>
 <body>
-This is normal font <br>
-<font face="Tahoma">
-This is Tahoma font
-</font>
+<font color="red">Color Names</font>
+<font color="rgb(255,99,71)">RGB values</font>
+<font color="#ff6347">HEX values</font>
+<font color="hsl(9,100%,64%)">HSL values</font>
+<font color="rgba(255,99,71,0.5)">RGBA values</font>
+<font color="hsla(8,100%,64%,0.5)">HSLA values</font>
 </body>
 </html>
